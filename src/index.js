c23b36c6bd5ea56c8b770691e091917840e53ce3
MariaPetrovskaia <mariapet70@gmail.com>
2024-10-17 21:30:46 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n).toString()
+  let l = num.length
+  let newNum = 0
+  for (let i = 0; i < l; i++) {
+    newNum += parseInt(num[i]) * (10 ** i)
+  }
+  return newNum
 }
+
